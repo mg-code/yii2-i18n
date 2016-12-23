4df60bbd4ebe831c221283c901fa04f8ca89fc73
Maris Graudins <maris@mg-interactive.lv>
2016-12-23 14:01:53 +0200

    language table

---
+++ console/migrations/m160513_105555_language_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\db\Migration;
+
+class m160513_105555_language_table extends Migration
+{
+    public function up()
+    {
+        $strOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $strOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+        $this->createTable('{{%language}}', [
+            'iso_code' => $this->char(2)->notNull(),
+            'title' => $this->string(255)->notNull(),
+            'sort' => $this->smallInteger(3)->unsigned()->notNull(),
+            'created_at' => $this->timestamp()->notNull()->defaultExpression('CURRENT_TIMESTAMP'),
+            'updated_at' => $this->dateTime()->notNull(),
+        ], $strOptions);
+
+        $this->addPrimaryKey('pk', '{{%language}}', ['iso_code']);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%language}}');
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
